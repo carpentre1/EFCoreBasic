745bc3055f8c3ee50354be566037d8da34e54ef7
carpentre1 <jlowell.carpenter@gmail.com>
2019-10-26 12:08:51 -0500

    Added nuget packages, Blogging code, better gitignore
    Added the SqlServer and Sqlite packages
    Added the BloggingContext, Blog, and Post classes
    Added a visual studio gitignore

--- EntityFrameworkCore/EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/EntityFrameworkCore/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace EntityFrameworkCore
 {
@@ -8,5 +10,39 @@ namespace EntityFrameworkCore
         {
             Console.WriteLine("Hello World!");
         }
+    }
+}
+
+
+namespace Intro
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Integrated Security=True");
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public int Rating { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
     }
 }
